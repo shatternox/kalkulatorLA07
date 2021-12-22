514de2d2c918b6e3750916c5a7784420d4d333e7
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2021-12-22 13:43:39 +0700

    menambahkan main file

---
+++ main.py
@@ -0,0 +1,45 @@
+
+def penambahan(a,b):
+    total = a + b
+    return total
+
+
+def main():
+    print(penambahan(10,5))
+    
+    
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
