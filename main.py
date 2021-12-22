821a7284078de097641eabff4d037c11ecaae8e4
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2021-12-22 14:18:37 +0700

    menambahkan function perkalian

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ def penambahan(a,b):
 def pengurangan(a,b):
     return a-b
 
+def perkalian(a,b):
+    return a*b
 
 def main():
     print(penambahan(10,5))
     print(pengurangan(10,5))
+    print(perkalian(10,5))
     
     
 if __name__ == "__main__":
